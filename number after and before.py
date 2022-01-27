0326448290c1a40da98ed334c10fb5abb624ee98
Daiheny <62754491+Daiheny@users.noreply.github.com>
2022-01-27 22:02:23 +0000

    Add files via upload

---
+++ number after and before.py
@@ -0,0 +1,13 @@
+num1 = int (input ('Type a number:'))
+print('The number typed is : {}'.format(num1))
+print ( '='*30)
+print ('Number {}'. format(num1 + 1),  (' comes after it'))
+print ('Number {}'.format(num1 -1), ('comes before it'))
+
+print ('Number {}'.format(num1*2), ('in case you want to double it'))
+print ('Number {}'.format(num1**3),(' as an exponent of 3'))
+print ('Number {}'.format(num1**(1/2)),('and guess what is this one?'))
+
+print ('='*50)
+print ('='*19, 'THANK YOU !', '='*18)
+    
