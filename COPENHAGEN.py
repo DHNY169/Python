904a5ee7ea8c2c3bee65308a943586fe91fca495
Daiheny <62754491+Daiheny@users.noreply.github.com>
2022-01-27 21:58:58 +0000

    Add files via upload

---
+++ COPENHAGEN.py
@@ -0,0 +1,8 @@
+print ('PROJECT COPENHAGEN')
+print ('='*20)
+name= (input ('Type your name: '))
+date= (input (' Type the month of traveling:'))
+money= float(input( 'Type how much EURO are you bringing to COPENHAGEN:'))
+
+print ('Dear', name,'\n'+ 'For your holidays on', date, 'in COPENHAGEN you will have {}'.format(money *7.45),' on Danish kroner.')
+print ('Enjoy your holiday,'+ name)
