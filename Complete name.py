5fb430c0bf81696955c6cae75beb183d59ca99df
Daiheny <62754491+Daiheny@users.noreply.github.com>
2022-01-27 22:00:20 +0000

    Add files via upload

---
+++ Complete name.py
@@ -0,0 +1,9 @@
+name = str(input('Type your Full Name: ').strip())
+n = name.split()
+print('Your first name is {}'.format(n[0]))
+print('Your last name is {}'.format(n[len(n)-1]))
+
+rep = str(input('Type a new Name for yourself'))
+print('Your new name is{}'n.replace(n[0], rep)))
+
+
